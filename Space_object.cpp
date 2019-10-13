d4201b2d106aff75cd797539d43087db34f7c87a
adam wojciechowski <adi61044@gmail.com>
2019-10-13 02:14:44 +0200

    Space_object class added

---
+++ Space_object.cpp
@@ -0,0 +1,5 @@
+#include "Space_object.h"
+
+Space_object::Space_object(int hp) {
+	this->hp = hp;
+}
---
+++ Space_object.h
@@ -0,0 +1,9 @@
+#ifndef SPACE_OBJECT_H
+#define SPACE_OBJECT_H
+class Space_object {
+	public:
+		int hp;
+		int pos_x, pos_y;
+		Space_object(int hp);
+};
+#endif
